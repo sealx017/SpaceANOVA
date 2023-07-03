b85a5420ec8024994a8893bd165eaee39c9c9f9d
sealx017 <48604757+sealx017@users.noreply.github.com>
2023-07-02 22:15:36 -0400

    Changes in data

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' T1DM IMC dataset
+#'
+#' An IMC dataset with six columns with cell level information as described below,
+#' and each row corresponding to an individual cell.
+#'
+#' @format A data frame with 1292937 rows (cells) and 6 variables (cell level info.):
+#' \describe{
+#'   \item{Group}{Group index}
+#'   \item{ID}{Subject index}
+#'   \item{imageID}{Imgae index}
+#'   \item{x}{x co-ordinate of the cell}
+#'   \item{y}{y co-ordinate of the cell}
+#'   \item{cellType}{Type of the cell}
+#' }
+#' @source \url{https://www.angelolab.com/mibi-data}
+"IMC_T1DM"
